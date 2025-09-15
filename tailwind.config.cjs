6c03c33dc9ca3305341e70a7551899357c763c13
patidarmk <34210081+patidarmk@users.noreply.github.com>
2025-09-15 11:01:31 +0530

    Add tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'applaa-blue': '#2563eb',
+        'applaa-purple': '#8b5cf6'
+      }
+    }
+  },
+  plugins: []
+};
